b46021e407be570e25c382087f9cde3022436324
Tijs Zwinkels <tijs@tinkertank.eu>
2023-02-01 13:57:41 +0100

    Some layout changes + generate button

--- src/Playground.tsx
+++ src/Playground.tsx
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Playground.css';
 
 function Playground() {
+    const [model, setModel] = useState('');
+    const [generating, setGenerating] = useState(false);
+    const [text, setText] = useState('');
+
+    const onGenerate = () => {
+        setGenerating(true);
+        console.log('Generating...');
+    };
+
     return (
         <div className="playground-container">
             <header>
                 <h1 className="title">Playground</h1>
                 <div className="dropdown-container">
-                    <select>
-                        <option value="Option 1">Option 1</option>
-                        <option value="Option 2">Option 2</option>
-                        <option value="Option 3">Option 3</option>
+                    <label htmlFor="model-select">Model:</label>
+                    <select
+                        id="model-select"
+                        value={model}
+                        onChange={e => setModel(e.target.value)}
+                    >
+                        <option value="">Select a model</option>
+                        <option value="model1">Model 1</option>
+                        <option value="model2">Model 2</option>
+                        <option value="model3">Model 3</option>
                     </select>
                 </div>
             </header>
-            <main>
-                <textarea className="textbox" />
-            </main>
+            <textarea
+                className="textbox"
+                value={text}
+                onChange={e => setText(e.target.value)}
+                readOnly={generating}
+                disabled={generating}
+            />
+            {generating ? (
+                <div className="overlay">
+                    <div className="spinner" />
+                </div>
+            ) : (
+                <button onClick={onGenerate}>Generate</button>
+            )}
         </div>
     );
 }
